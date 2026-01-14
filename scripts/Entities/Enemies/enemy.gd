3e5f4656ae511dea9fdcba9211e6189ed44cd5fd
Antoine Nisoli <nisoliantoine@outlook.com>
2026-01-14 17:21:17 +0100

    enemy is entity base class

--- scripts/Entities/Enemies/enemy.gd
+++ scripts/Entities/Enemies/enemy.gd
@@ -1,37 +1,13 @@
-extends CharacterBody2D
+extends Entity
 
-@export var sprite: AnimatedSprite2D
-@export var hitColor: Color
-@export var min_distance: float = 50
 @export var _stats: Stats
-
 @onready var player = get_tree().get_nodes_in_group("Player")[0]
-
-func _ready() -> void:
-	_stats.entity_death.connect(death)
-	await get_tree().process_frame
-	print(player)
-	print(_stats)
-
-func hit_flash() -> void:
-	sprite.self_modulate = hitColor
-	await get_tree().create_timer(0.1).timeout
-	sprite.self_modulate = Color.WHITE # reset to default
-
-func takeDmg() -> void:
-	_stats.current_hp -= 1
-	print(_stats.current_hp)
-	hit_flash()
-
-func death() -> void:
-	print("i'm dead!!")
-	queue_free()
 
 func _physics_process(_delta: float):
 	var dir = position.distance_to(player.position)
 	#print(dir)
 
-	if dir > min_distance:
+	if dir > _stats.min_distance:
 		sprite.play("move")
 		var direction: Vector2 = (player.position - position).normalized()
 		velocity = direction * _stats.move_speed
--- scripts/Entities/entity.gd
+++ scripts/Entities/entity.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 class_name Entity
 
+@export var _health: Health
 @export var sprite: AnimatedSprite2D
 @export var hitColor: Color
-@export var _health: Health
 
 func _ready() -> void:
 	_health.on_death.connect(death)
 	await get_tree().process_frame
-	print(_health)
 
 func hit_flash() -> void:
 	sprite.self_modulate = hitColor
@@ -17,9 +16,7 @@ func hit_flash() -> void:
 
 func takeDmg() -> void:
 	_health.current_hp -= 1
-	print(_health.current_hp)
 	hit_flash()
 
 func death() -> void:
-	print("i'm dead!!")
 	queue_free()

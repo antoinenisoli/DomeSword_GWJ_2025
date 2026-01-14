36a4ecc2ea1017ec5cc51781a2ceffd522acfdb6
Antoine Nisoli <nisoliantoine@outlook.com>
2026-01-14 16:47:38 +0100

    lerp turret rotation

--- scripts/Entities/player.gd
+++ scripts/Entities/player.gd
@@ -4,7 +4,7 @@ extends StaticBody2D
 @export var reload_timer: Timer
 @export var shootPos: Node2D
 @export var rotate_speed: float = 150
-@export var rot_limit: float = 110
+@export var rot_limit: float = 70
 
 func shoot() -> void:
 	if !reload_timer.is_stopped() || !bullet:
@@ -18,17 +18,25 @@ func shoot() -> void:
 		newB.rotation = shootPos.global_rotation
 		reload_timer.start()
 
-func control_turret(_delta: float) -> void:
+func rotate_turret(_delta: float) -> void:
 	var mousePos = get_global_mouse_position()
-	var rot = transform.looking_at(mousePos).get_rotation()
-	rot += deg_to_rad(90)
-	var f = fposmod(rad_to_deg(rot), -360)
-	print(rad_to_deg(rot))
-	rotation = rot
+	var v = mousePos - global_position
+	var angle = v.angle()
+	var r = global_rotation
+
+	#lerp angle towards the mouse
+	var angle_delta = rotate_speed * _delta
+	angle = lerp_angle(r, angle + deg_to_rad(90), 1.0)
+	angle = clamp(angle, r - angle_delta, r + angle_delta)
+	global_rotation = angle
+
+	#limit the rotation
+	var limit = deg_to_rad(rot_limit)
+	global_rotation = clamp(global_rotation, -limit, limit)
 
 func _process(_delta):
 	#print(reload_timer.is_stopped())
-	control_turret(_delta)
+	rotate_turret(_delta)
 	if Input.is_action_pressed("fire"):
 		shoot()
 

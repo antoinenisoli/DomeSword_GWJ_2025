ea30c9c31707c14174c897a3dcac1c98423ab96d
Antoine Nisoli <nisoliantoine@outlook.com>
2026-01-18 13:57:39 +0100

    pause screen

--- scripts/UI/ui_controller.gd
+++ scripts/UI/ui_controller.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var enemy_txt: Label
 @export var flame_txt: Label
+@export var pause_screen: Control
+@export var hud_screen: Control
 var enemy_killed: int
 
 func _ready() -> void:
@@ -11,3 +13,12 @@ func _ready() -> void:
         enemy_killed += 1
         enemy_txt.text = str(enemy_killed)
         )
+
+func set_paused(b: bool) -> void:
+    var t = 0 if b else 1
+    TimeManager.set_time(t)
+    pause_screen.visible = b
+
+func _process(_delta):
+    if Input.is_action_just_pressed("pause_game"):
+        set_paused(!pause_screen.visible)

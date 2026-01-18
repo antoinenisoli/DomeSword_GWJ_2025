f519dbff5656d54078038df25c83a4a7e17e6a56
Antoine Nisoli <nisoliantoine@outlook.com>
2026-01-18 04:53:21 +0100

    camera shaker

--- scripts/camera/camera_controller.gd
+++ scripts/camera/camera_controller.gd
@@ -1,14 +1,29 @@
 extends Camera2D
 
+@export var shakers: Dictionary[String, CameraShake]
 @export var shake_duration = 0.1
 @export var shake_amount = 10
 
 var current_shake = 0
 
 func _ready():
-    EventManager.on_player_damaged.connect(shake)
+    EventManager.on_player_damaged.connect(func f(_t = 0) -> void:
+        play_shaker("player_hit"))
+
     EventManager.on_sword_hit.connect(func f(_dmg: int) -> void:
-        shake())
+        play_shaker("sword_hit"))
+
+func play_shaker(_name: String) -> void:
+    if !shakers.has(_name):
+        print(_name + " doesn't exist.")
+        return
+
+    var shaker: CameraShake = shakers.get(_name)
+    use_shaker(shaker)
+
+func use_shaker(shaker: CameraShake) -> void:
+    current_shake = shaker.shake_amount
+    shake_duration = shaker.shake_duration
 
 func shake(_t = 0) -> void:
     current_shake = shake_amount
---
+++ scripts/camera/camera_shake.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name CameraShake
+
+@export var shake_duration = 0.1
+@export var shake_amount = 10

5842e27dccb6847a043f8ab6a481f9272270ebec
Antoine Nisoli <nisoliantoine@outlook.com>
2026-01-18 16:29:20 +0100

    tween floating txt alpha

--- scripts/UI/floating_text.gd
+++ scripts/UI/floating_text.gd
@@ -8,12 +8,15 @@ func _ready():
     call_deferred("play_tween")
 
 func play_tween() -> void:
-    var tween := create_tween()
+    var tween := create_tween().set_parallel(true)
     tween.tween_property(self, "position", global_position + up_offset, tween_duration)
-    #tween.tween_property(txt, "color.a", 0, tween_duration)
+    var c = Color.WHITE
+    c.a = 0
+    tween.tween_property(txt, "theme_override_colors/default_color", c, tween_duration)
     tween.play()
+    
     await tween.finished
     queue_free()
 
 func set_text(content: String) -> void:
-    txt.text = "+" + content
+    txt.text = "+" + content + " ammo"
